ca8d10b1c8de0598429c3ff06e24173b333f314a
Rahul Roy <rahulraghav6385@gmail.com>
2026-06-15 20:14:00 +0530

    added date and checkbox functionality

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 5550;
+const PORT = 5566;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -18,14 +18,26 @@ app.get('/',(req,res)=>{
 
 app.post('/add-task',(req,res)=>{
     const task = req.body.task;
-
-    if (task && task.trim() !== "") {
-        tasks.push(task);
-    }
-
+    const dueDate = req.body.dueDate;
+        
+        tasks.push({
+            task: task,
+            completed: false,
+            dueDate:dueDate,
+        });
     return res.redirect('/');
 
+
 })
+
+app.post('/delete-task/:index',(req,res)=>{
+    const index = parseInt(req.params.index);
+
+        tasks.splice(index, 1);
+        
+    res.redirect('/');
+});
+
 
 
 
