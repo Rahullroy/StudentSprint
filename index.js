0127f86d9b6c4582f2568e6123a09f956bacc993
Rahul Roy <rahulraghav6385@gmail.com>
2026-06-16 18:44:05 +0530

    Added task persistence with JSON storage and due date support

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 5586;
+const PORT = 5546;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -10,7 +10,15 @@ app.set("view engine","ejs");
 app.set("views",path.resolve("./views"));
 app.use(express.static("public"));
 
-let tasks =[];
+let fileContent = fs.readFileSync('./tasks.json','utf-8');
+let tasks= JSON.parse(fileContent);
+
+
+function saveTasks(){
+        let taskstring = JSON.stringify(tasks);
+        fs.writeFileSync('./tasks.json',taskstring);
+
+    }
 
 app.get('/',(req,res)=>{
     res.render("index",{tasks:tasks});
@@ -19,13 +27,14 @@ app.get('/',(req,res)=>{
 app.post('/add-task',(req,res)=>{
     const task = req.body.task;
     const dueDate = req.body.dueDate;
-        
+    
         tasks.push({
             task: task,
             completed: false,
             dueDate:dueDate,
         });
-    return res.redirect('/');
+    saveTasks();
+    res.redirect('/');
 
 
 })
@@ -33,7 +42,9 @@ app.post('/add-task',(req,res)=>{
 app.post('/delete-task/:index',(req,res)=>{
     const index = parseInt(req.params.index);
 
-        tasks.splice(index, 1);
+    tasks.splice(index, 1);
+    saveTasks();
+    
 
     res.redirect('/');
 });
@@ -43,11 +54,16 @@ app.post('/complete-task/:index',(req,res)=>{
      if(index >= 0 && index < tasks.length){
         tasks[index].completed = !tasks[index].completed;
     }
+    saveTasks();
 
     res.redirect('/');
 
 
 })
+ 
+
+
+
 
 
 
